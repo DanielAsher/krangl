5952412c40cf87bae25a0375ef5e90c004f74853
Daniel Asher <daniel.asher.2303@gmail.com>
2020-02-05 20:12:15 +0000

    Remove `=` operator from IterableDataFrameBuilder for android build

--- src/main/kotlin/krangl/typed/Build.kt
+++ src/main/kotlin/krangl/typed/Build.kt
@@ -17,7 +17,7 @@ class IterableDataFrameBuilder<T>(val source: Iterable<T>){
 
     inline infix fun <reified R> String.to(noinline expression: T.() -> R?) = add(this, expression)
 
-    inline infix fun <reified R> String.`=`(noinline expression: T.() -> R?) = add(this, expression)
+    // inline infix fun <reified R> String.`=`(noinline expression: T.() -> R?) = add(this, expression)
 
     inline infix operator fun <reified R> String.invoke(noinline expression: T.() -> R?) = add(this, expression)
 }
